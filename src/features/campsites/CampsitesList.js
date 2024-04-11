1af02e8df76a1182561b7ca91b1c94e2d81b2af6
Vincent <vstella215@gmail.com>
2024-04-11 00:33:43 -0400

    useState

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -3,14 +3,18 @@ import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
   const CampsitesList = selectAllCampsites();
 
   return (
     <Row className="ms-auto">
       {CAMPSITES.map((campsite) => {
         return (
-          <Col md='5' className='m-4' key={campsite.id}>
+          <Col md='5' 
+          className='m-4' 
+          key={campsite.id}
+          onClick={() => setCampsiteId(campsite.id)}
+          >
             <CampsiteCard campsite={campsite} />
           </Col>
         );
