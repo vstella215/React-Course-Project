fefd413f89f6a11e1dd3ba6eeb232361bdb1adea
Vincent <vstella215@gmail.com>
2024-04-12 16:17:35 -0400

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
   return (
@@ -15,6 +16,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route 
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage/>}
+        />
       </Routes>
       <Footer />
     </div>
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -3,18 +3,14 @@ import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
   const CampsitesList = selectAllCampsites();
 
   return (
     <Row className="ms-auto">
       {CAMPSITES.map((campsite) => {
         return (
-          <Col md='5' 
-          className='m-4' 
-          key={campsite.id}
-          onClick={() => setCampsiteId(campsite.id)}
-          >
+          <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />
           </Col>
         );
