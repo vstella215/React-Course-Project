bf36b2645d52bbe44a5630fbc5ebb75bfa072b2f
Vincent <vstella215@gmail.com>
2024-04-12 00:14:44 -0400

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -7,3 +7,8 @@ export const selectAllCampsites = () => {
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === id);
 }
+
+export const selectFeaturedCampsite =() => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
+
