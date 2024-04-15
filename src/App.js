b0e81af89a3cdfcffbb3a0b84cf9657637a91b6c
Vincent <vstella215@gmail.com>
2024-04-15 01:03:35 -0400

    Partners List

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           path='directory/:campsiteId'
           element={<CampsiteDetailPage/>}
         />
+        <Route path='about' element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,21 @@
+import { Col } from 'reactstrap';
+import Partner from './Partner';
+import { selectAllPartner } from './partnersSlice';
+
+const  PartnersList = () => {
+    const partners = selectAllPartner();
+
+    return (
+        <Col className='mt-4'>
+            {partners.map((partners) => {
+                return (
+                    <div className='d-flex mb-5' key={partners.id}>
+                        <Partner partner={partners} />
+                    </div>
+                )
+            })}
+        </Col>
+    )
+}
+
+export default PartnersList
