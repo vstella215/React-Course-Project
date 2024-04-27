771c43600327dd3c580f0bca19e76d8eda4e3dc9
Vincent <vstella215@gmail.com>
2024-04-27 13:10:41 -0400

    Login Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ import {
 } from "reactstrap";
 import { NavLink } from 'react-router-dom';
 import NucampLogo from "../app/assets/img/logo.png";
+import UserLoginForm from '../features/user/UserLoginForm';
 
 
 const Header = () => {
@@ -48,6 +49,9 @@ const Header = () => {
             <i className='fa fa-address-card fa-lg' /> Contact
             </NavLink>
 
+          </NavItem>
+          <NavItem>
+            <UserLoginForm />
           </NavItem>
         </Nav>
       </Collapse>
---
+++ src/utils/validateUserLoginForm.js
@@ -0,0 +1,19 @@
+const validateUserLoginForm = (values) => {
+    const errors = {};
+
+    if (!values.username) {
+        errors.username = 'Username us required';
+    } else if (values.username.length < 6 || values.username.length > 15) {
+        errors.username = 'Username must be between 6 and 15 characters';
+    }
+
+    if (!values.password) {
+        errors.password = 'Password is required';
+    } else if (values.password.length < 8) {
+        errors.password = 'Password must be at least 8 characters';
+    }
+
+    return errors;
+};
+
+export default validateUserLoginForm
