b6b8bbb868a86335363614cddac8933442656802
Vincent <vstella215@gmail.com>
2024-05-04 11:40:43 -0400

    Async Comments

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import {Button, Modal, ModalHeader, ModalBody, FormGroup, Label} from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import {validateCommentForm} from '../../utils/validateCommentForm';
-import { addComment } from "./commentsSlice";
+import { addComment, postComments } from "./commentsSlice";
 
 
 const CommentForm = ({ campsiteId }) => {
@@ -20,6 +20,7 @@ const CommentForm = ({ campsiteId }) => {
         };
         console.log(comment);
         dispatch(addComment(comment));
+        dispatch(postComments(comment));
         setModalOpen(false);
     };
 
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -3,9 +3,30 @@ import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selecCommentsByCampiteId } from './commentsSlice';
 import CommentForm from './CommentForm';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = useSelector(selecCommentsByCampiteId(campsiteId));
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector((state) => state.comments.errMsg);
+
+    if (isLoading) {
+        return (
+          <Col md='5' className='m-1'>
+            <Loading />
+          </Col>
+        );
+      }
+    
+      if (errMsg) {
+        return (
+          <Col md='5' className='m-1'>
+            <Error errMsg={errMsg} />
+          </Col>
+        );
+      }
+    
 
     if (comments && comments.length > 0) {
         return (
